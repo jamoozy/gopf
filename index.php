3fe386208a9f9ece579d8fd095c4f0a824f0b2d1
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2011-12-25 11:45:49 -0500

    Added GPL headers (finally).  I have been putting it off much too long ...

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-<!DOCTYPE html>
 <?php
+// Copyright 2012 Andrew "Jamoozy" Correa
+//
+// This file is part of GOPF.
+//
+// GOPF is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Foobar is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GOPF. If not, see http://www.gnu.org/licenses/.
+
 include("mysql.php");
 include("list.php");
 
 if (!($ip_error = ip_is_ok($_SERVER["REMOTE_ADDR"]))) {
 ?>
 
+<!DOCTYPE html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -70,7 +87,7 @@ if (!($ip_error = ip_is_ok($_SERVER["REMOTE_ADDR"]))) {
 <?php
 } else {
 
-// The following is all done inline (as opposed to having separate CSS 
+// The following is all done inline (as opposed to having separate CSS
 // and JS files) to decrease complexity.
 ?>
 <html>
