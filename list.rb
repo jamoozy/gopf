91a89dfbafd12b9816669d13b0f771050782330d
Andrew "Jamoozy" Correa <jamoozy@gmail.com>
2014-04-14 20:42:23 -0400

    Got JSON / text server script working.

--- list.rb
+++ list.rb
@@ -17,61 +17,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with GOPF. If not, see http://www.gnu.org/licenses/.
 
+
 require 'cgi'
 
-$playlist_dir = "data/playlists/";
-
-# Generates playlist from playlist files.  Playlist files are simple text
-# files with each line containing the relative path to a song.
-#
-# playlist: String name of playlist to start selected.
-def generate_playlists(playlist)
-  fnames = Dir[File.join(File.dirname(__FILE__), $playlist_dir, '*')].reject{ |f| f[0] == '.' or File.executable?(f) or f[-1] == "~"}.sort
-
-  rtn = '';
-  if playlist
-    fnames.each do |fname|
-      if playlist == fname
-        rtn << "<li class=\"unselected\">#{fname}</li>"
-      else
-        rtn << "<li class=\"unselected selected\" id=\"selected\">#{fname}</li>"
-      end
-    end
-  else
-    fnames.each {|fname| rtn << "<li class=\"unselected\">#{fname}</li>"}
-  end
-  return $rtn;
-end
-
-def generate_media(playlist, media=false)
-  paths = File.readlines(File.join($playlist_dir, playlist)).map{|f| f.strip}
-  html = '';
-  paths.reject{|p| p.size == 0}.each do |path|
-    name = path[path.rindex('/')...-4]
-    html << "<li class=\"media#{name == media ? ' playing' : ''}\" path=\"#{path.sub('..', 'data')}\" onclick=\"media.onclick(this)\">#{name}</li>\n";
-  end
-
-  return html;
-end
 
 if __FILE__ == $0
+  $playlist_dir = "data/playlists/";
   $cgi = CGI.new
 
-  if $cgi.params.has_key?('op')
-    if $cgi.params['op'] == 'ls'
-      if $cgi.params.has_key?('dir')
-        #ls($_GET['dir']);
-        $cgi.out("text/json"){'{error:"ls not implemented."}'}
-      else
-        $cgi.out("text/json"){'{error:"Expected \"dir\" key."}'}
-      end
-    end
-  end
-
   # Contents of return page are just text.  Playlist must be in utf-8.
-  if $cgi.params.has_key? 'playlist'
-    $cgi.out("text/plain") do
-      File.readlines(File.join($playlist_dir, $cgi.params['playlist'])).join
+  if $cgi.params.has_key?('playlist')
+    playlist = File.join($playlist_dir, $cgi.params['playlist'])
+    if File.exists?("#{playlist}.json")
+      $cgi.out("text/json"){File.readlines("#{playlist}.json").join}
+    elsif File.exists?(playlist)
+      $cgi.out("text/plain"){File.readlines(playlist).join}
+    else
+      $cgi.out("text/json"){"{error:'No such file: \"#{playlist}\"'"}
     end
   end
 end
